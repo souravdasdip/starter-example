efd4fc411bd682f41f1a82a4a345db6ce49615e0
Sourav Das <souravdasdip65@gmail.com>
2023-11-28 18:55:18 +0600

    9.Streaming

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,4 +1,4 @@
-import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
 import { generateYAxis } from '@/app/lib/utils';
 import { lusitana } from '@/app/ui/fonts';
 import { CalendarIcon } from '@heroicons/react/24/outline';
@@ -9,13 +9,10 @@ import { CalendarIcon } from '@heroicons/react/24/outline';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
+  const revenue = await fetchRevenue(); // Fetch data inside the component
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
